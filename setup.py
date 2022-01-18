8a778b46a39d8b13522143a63798121eedfca387
Kevin Unkrich <kevinunkrich@ip-192-168-4-93.us-west-2.compute.internal>
2022-01-18 12:38:11 -0800

    Update version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 import pkg_resources
 from setuptools import setup, find_namespace_packages
 
-VERSION = '0.0.3'
+VERSION = '0.0.2.post1'
 
 with open('README.md') as f:
     readme = f.read()
