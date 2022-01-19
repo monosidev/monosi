d68d75800e4956e0fb1b198451fc5432a74e68d4
Kevin Unkrich <kevinunkrich@Kevins-MacBook-Pro-2.local>
2022-01-19 15:51:12 -0800

    Resolve scheduler startup error on task creation

--- monosi/tasks/schedule.py
+++ monosi/tasks/schedule.py
@@ -1,19 +1,34 @@
+from monosi.config.configuration import Configuration
 from monosi.events import track_event
-from monosi.tasks.base import TaskBase
+from monosi.tasks.base import ProjectTask
 from monosi.tasks.run import RunnerTask
 
 from monosi.scheduler.job import MonitorJob
 from monosi.scheduler.manager import JobManager
 
-class ScheduleMonitorsTask(TaskBase):
-    def __init__(self, args, config, manager, monitors):
-        super.__init__(args, config)
+class ScheduleMonitorsTask(ProjectTask):
+    def __init__(self, args, config, manager: JobManager):
+        super().__init__(args, config)
         self.manager = manager
-        self.monitors = monitors
 
-    def run(self, *args, **kwargs):
-        track_event(self.config, action="scheduling_monitors")
-        for monitor in self.monitors:
-            job = RunnerTask(self.args, self.config, [monitor])
-            self.manager.add_job(job, self.args, minutes=monitor.schedule.minutes)
+    def _create_tasks(self):
+        return []
+
+    def _process_tasks(self):
+        if self.project is not None:
+            track_event(self.config, action="scheduling_monitors")
+
+            for monitor in self.project.monitors:
+                task = RunnerTask(self.args, self.config, [monitor])
+                job = MonitorJob(task=task)
+                self.manager.add_job(job, self.args, minutes=monitor.schedule.minutes)
+
+    @classmethod
+    def from_args(cls, args, manager: JobManager):
+        try:
+            config = Configuration.from_args(args)
+        except:
+            raise Exception("There was an issue creating the task from args.")
+
+        return cls(args, config, manager)
 
