48644b6b5848b47b9dae35c34cff1ecbb8bab042
Kevin Unkrich <kevinunkrich@ip-192-168-4-93.us-west-2.compute.internal>
2022-02-06 21:30:48 -0800

    WIP: Frontend thresholds

--- src/ui/src/pages/app/monitors/Index/components/MonitorForm/CustomConfiguration.tsx
+++ src/ui/src/pages/app/monitors/Index/components/MonitorForm/CustomConfiguration.tsx
@@ -1,13 +1,52 @@
 import React, { useState } from 'react';
 
-import { EuiFormRow, EuiFieldText, EuiTextArea } from '@elastic/eui';
+import {
+  EuiFormRow,
+  EuiFieldText,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiComboBox,
+} from '@elastic/eui';
 
-const CustomConfiguration: React.FC<{ setConfiguration: any }> = ({
-  setConfiguration,
-}) => {
+const ThresholdForm: React.FC = () => {
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [value, setValue] = useState('');
+
+  return (
+    <EuiFlexGroup>
+      <EuiFlexItem>
+      <EuiFormRow label="Operator">
+      <EuiComboBox
+          placeholder="Select a single option"
+          singleSelection={{ asPlainText: true }}
+          options={[
+            {
+              label: 'eq',
+              value: 'eq'
+            },
+          ]}
+          selectedOptions={selectedOptions}
+          onChange={(e:any) => setSelectedOptions(e.target.value)}
+        />
+      </EuiFormRow>
+      </EuiFlexItem>
+      <EuiFlexItem>
+      <EuiFormRow label="Value">
+        <EuiFieldText
+          disabled
+          value={value}
+          onChange={(e: any) => setValue(e.target.value)}
+        />
+      </EuiFormRow>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+}
+
+const CustomConfiguration: React.FC<{setConfiguration: any}> = ({setConfiguration}) => {
   const [sql, setSql] = useState('');
-  const [thresholds, setThresholds] = useState('');
-
+  const [thresholds, setThresholds] = useState([]);
+  
   function updateFormState(setState: any) {
     setState();
     setConfiguration({ sql: sql, thresholds: [] });
