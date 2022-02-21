4e20d462621fdf3fd4ba3cfcbe4ef2562d33d03b
Kevin Unkrich <kevin@unkri.ch>
2022-02-21 14:17:50 -0800

    Updates to resolve rebase against master

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 import os
+import sys
+
+curr_path = os.path.dirname(os.path.abspath(__file__))
+src_path = os.path.abspath(os.path.join(curr_path, "../"))
+sys.path.append(src_path)
 
 from .api import init_api
 from .db import init_db
