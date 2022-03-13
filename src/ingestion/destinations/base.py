3fe114a3276e98540a12b090f9aa7ece5130d2ff
Kevin Unkrich <kevin@unkri.ch>
2022-03-12 17:06:44 -0800

    Minor syntax changes

--- src/ingestion/destinations/base.py
+++ src/ingestion/destinations/base.py
@@ -1,51 +1,52 @@
+from dataclasses import dataclass
+import abc
+import json
 
 @dataclass
 class DestinationConfiguration:
-	name: str
-	configuration: str
-	enabled: bool = True
+    name: str
+    configuration: str
+    enabled: bool = True
 
-	def __init__(self, configuration: Dict[str, Any]):
-		self.configuration = configuration
+    def __init__(self, configuration: str):
+        self.configuration = configuration
 
-	@classmethod
-	def validate(cls, configuration):
-		raise NotImplementedError
+    @classmethod
+    def validate(cls, configuration):
+        raise NotImplementedError
 
-	def connection_string(self):
-		raise NotImplementedError
+    def connection_string(self):
+        raise NotImplementedError
 
-	@abc.abstractproperty
-	def type(self):
-		raise NotImplementedError
+    @abc.abstractproperty
+    def type(self):
+        raise NotImplementedError
 
-	def to_dict(self):
-		return {
-			"name": self.name,
-			"configuration": json.loads(self.configuration),
-			"enabled": self.enabled,
-			"type": self.type,
-		}
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "configuration": json.loads(self.configuration),
+            "enabled": self.enabled,
+            "type": self.type,
+        }
 
 class Destination(object):
-	"""docstring for Destination"""
-	def __init__(self, arg):
-		super(Destination, self).__init__()
-		self.configuration = configuration
+    def __init__(self, configuration):
+        self.configuration = configuration
 
-	def _before_push(self):
-		pass
+    def _before_push(self):
+        pass
 
-	def _after_push(self):
-		pass
+    def _after_push(self):
+        pass
 
     @abc.abstractmethod
-	def _push(self):
-		raise NotImplementedError
+    def _push(self, data):
+        raise NotImplementedError
 
-	def push(self):
-		self._before_push()
-		results = self._push()
-		self._after_push()
+    def push(self, data):
+        self._before_push()
+        results = self._push(data)
+        self._after_push()
 
-		return results
+        return results
--- src/ingestion/sources/base.py
+++ src/ingestion/sources/base.py
@@ -1,72 +1,83 @@
+from dataclasses import dataclass
 import abc
 import json
 
 from typing import Any, Dict, List
 
+from ingestion.task import Task, TaskUnit
+
 
 @dataclass
 class SourceConfiguration:
-	name: str
-	configuration: str
-	enabled: bool = True
+    name: str
+    configuration: str
+    enabled: bool = True
 
-	def __init__(self, configuration: Dict[str, Any]):
-		self.configuration = configuration
+    @classmethod
+    def validate(cls, configuration: str):
+        raise NotImplementedError
 
-	@classmethod
-	def validate(cls, configuration):
-		raise NotImplementedError
+    def connection_string(self):
+        raise NotImplementedError
 
-	def connection_string(self):
-		raise NotImplementedError
+    @abc.abstractproperty
+    def type(self):
+        raise NotImplementedError
 
-	@abc.abstractproperty
-	def type(self):
-		raise NotImplementedError
-
-	def to_dict(self):
-		return {
-			"name": self.name,
-			"configuration": json.loads(self.configuration),
-			"enabled": self.enabled,
-			"type": self.type,
-		}
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "configuration": json.loads(self.configuration),
+            "enabled": self.enabled,
+            "type": self.type,
+        }
 
 class Source(object):
-	"""docstring for Source"""
-	def __init__(self, configuration: SourceConfiguration):
-		super(Source, self).__init__()
-		self.configuration = configuration
+    """docstring for Source"""
+    def __init__(self, configuration: SourceConfiguration):
+        super(Source, self).__init__()
+        self.configuration = configuration
 
-	def _before_pull(self):
-		pass
+    def _before_pull(self):
+        pass
 
-	def _after_pull(self):
-		pass
+    def _after_pull(self):
+        pass
 
     @abc.abstractmethod
-	def _pull(self):
-		raise NotImplementedError
+    def _pull(self):
+        raise NotImplementedError
 
-	def pull(self):
-		self._before_pull()
-		results = self._pull()
-		self._after_pull()
+    @abc.abstractmethod
+    def extractor(self):
+        raise NotImplementedError
 
-		return results
+    @abc.abstractmethod
+    def task_units(self):
+        raise NotImplementedError
 
+    def pull(self):
+        self._before_pull()
+        results = self._pull()
+        self._after_pull()
 
-class SQLAlchemyExtractor:
+        return results
+
+class Extractor(object):
+    def run(self, request: str):
+        raise NotImplementedError
+
+class SQLAlchemyExtractor(Extractor):
     def __init__(self, configuration):
-        self.config = config
+        self.configuration = configuration
         self.driver = None
 
     def _initialize(self):
         try:
             from core.drivers.factory import load_driver
-            driver_cls = load_driver(self.config)
+            driver_cls = load_driver(self.configuration)
 
-            self.driver = driver_cls(self.config)
+            self.driver = driver_cls(self.configuration)
         except Exception as e:
             print(e)
             raise Exception("Could not initialize connection to database in Runner.")
@@ -78,7 +89,7 @@ class SQLAlchemyExtractor:
         results = self.driver.execute(sql)
         return results
 
-    def run(self, unit: str):
+    def run(self, unit: TaskUnit):
         self._initialize()
         sql = unit.request
 
@@ -91,7 +102,7 @@ class SQLAlchemySourceDialect:
     
     @classmethod
     def _approx_distinctness(cls):
-        return "{} / CAST(COUNT(*) AS NUMERIC)".format(cls.approx_distinct_count())
+        return "{} / CAST(COUNT(*) AS NUMERIC)".format(cls._approx_distinct_count())
 
     @classmethod
     def _numeric_mean(cls):
@@ -111,98 +122,93 @@ class SQLAlchemySourceDialect:
 
     @classmethod
     def _mean_length(cls):
-        return cls.numeric_mean().format("LENGTH({})")
+        return cls._numeric_mean().format("LENGTH({})")
 
     @classmethod
     def _max_length(cls):
-        return cls.numeric_max().format("LENGTH({})")
+        return cls._numeric_max().format("LENGTH({})")
 
     @classmethod
     def _min_length(cls):
-        return cls.numeric_min().format("LENGTH({})")
+        return cls._numeric_min().format("LENGTH({})")
 
     @classmethod
     def _std_length(cls):
-        return cls.numeric_std().format("LENGTH({})")
+        return cls._numeric_std().format("LENGTH({})")
 
     @classmethod
     def _text_int_rate(cls):
-    	raise NotImplementedError
+        raise NotImplementedError
 
     @classmethod
     def _text_number_rate(cls):
-    	raise NotImplementedError
+        raise NotImplementedError
 
     @classmethod
     def _text_uuid_rate(cls):
-    	raise NotImplementedError
+        raise NotImplementedError
 
     @classmethod
     def text_all_spaces_rate(cls):
-    	raise NotImplementedError
+        raise NotImplementedError
 
     @classmethod
     def _text_null_keyword_rate(cls):
-    	raise NotImplementedError
+        raise NotImplementedError
 
     @classmethod
     def _zero_rate(cls):
-    	raise NotImplementedError
+        raise NotImplementedError
 
     @classmethod
     def _negative_rate(cls):
-    	raise NotImplementedError
+        raise NotImplementedError
 
     @classmethod
     def _completeness(cls):
-    	raise NotImplementedError
+        raise NotImplementedError
 
     @classmethod
     def schema_query(cls):
-    	raise NotImplementedError
+        raise NotImplementedError
 
     @classmethod
     def table_metrics_query(cls):
-    	raise NotImplementedError
+        raise NotImplementedError
 
     @classmethod
     def query_access_logs_query(cls):
-    	raise NotImplementedError
-    	
+        raise NotImplementedError
+        
     @classmethod
     def query_copy_logs_query(cls):
-    	raise NotImplementedError
+        raise NotImplementedError
 
 
 @dataclass
 class SQLAlchemySource(Source):
-	dialect: SQLAlchemySourceDialect
+    dialect: SQLAlchemySourceDialect
 
-	def _schema(self) -> TaskUnit:
-		return TaskUnit(request=self.dialect.schema_query())
+    def _schema(self) -> TaskUnit:
+        return TaskUnit(request=self.dialect.schema_query())
 
-	def _metrics(self) -> List[TaskUnit]:
-		tables = []
-		return [TaskUnit(request=self.dialect.table_metrics_query(table)) for table in tables]
+    def _metrics(self) -> List[TaskUnit]:
+        tables = [] # TODO: Get for all tables
+        return [TaskUnit(request=self.dialect.table_metrics_query()) for table in tables]
 
-	def _query_access_logs(self) -> TaskUnit:
-		return TaskUnit(request=self.dialect.query_access_logs_query())
+    def _query_access_logs(self) -> TaskUnit:
+        return TaskUnit(request=self.dialect.query_access_logs_query())
 
-	def _query_copy_logs(self) -> TaskUnit:
-		return TaskUnit(request=self.dialect.query_copy_logs_query())
+    def _query_copy_logs(self) -> TaskUnit:
+        return TaskUnit(request=self.dialect.query_copy_logs_query())
 
-	def task_units(self) -> List[TaskUnit]:
-		units = [
-			self._schema(),
-			self._query_access_logs(),
-			self._query_copy_logs()
-		]
-		[units.append(unit) for unit in self._metrics()]
+    def task_units(self) -> List[TaskUnit]:
+        units = [
+            self._schema(),
+            self._query_access_logs(),
+            self._query_copy_logs()
+        ]
+        [units.append(unit) for unit in self._metrics()]
 
-		return units
+        return units
 
-	def tasks(self):
-		extractor = SQLAlchemyExtractor(self.configuration)
-		units = self.task_units()
-
-		return [Task(extractor=extractor, units=unit) for unit in units]
