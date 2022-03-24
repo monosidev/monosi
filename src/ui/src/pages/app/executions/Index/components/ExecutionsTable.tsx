8d5ac88cab2bc6cc8539e4c2a899c9a78fd17f1d
Ivan Porollo <iporollo@gmail.com>
2022-03-24 10:08:44 -0700

    add loading state to executions table

--- src/ui/src/pages/app/executions/Index/components/ExecutionsTable.tsx
+++ src/ui/src/pages/app/executions/Index/components/ExecutionsTable.tsx
@@ -12,13 +12,17 @@ import { formatTimestamp } from 'utils/timestampFormatting';
 
 const ExecutionsTable: React.FC = () => {
   const [executions, setExecutions] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
+    // TODO: Move this to top level component and pass executions down as props
     async function loadExecutions() {
+      setIsLoading(true);
       let res = await ExecutionService.getAll();
       if (res !== null && res.executions) {
         setExecutions(res.executions);
       }
+      setIsLoading(false);
     }
 
     loadExecutions();
@@ -49,29 +53,43 @@ const ExecutionsTable: React.FC = () => {
       </div>
     )
   }
-  if (executions.length == 0) {
-        return emptyState();
+
+  const loadingState = () => (
+    <div className="p-5 mb-4 rounded-3" style={{background: '#f1f1f1'}}>
+      <div className="container-fluid py-5">
+        <h1 className="display-5 fw-bold">Loading job executions.</h1>
+        <p className="col-md-8 fs-4">This may take a few seconds.</p>
+      </div>
+    </div>
+  );
+
+  if(isLoading) {
+    return loadingState();
+  } else if (executions.length === 0) {
+    return emptyState();
+  } else {
+    return(
+      <ToolkitProvider
+          keyField="id"
+          data={executions}
+          columns={columns}
+          >
+          {
+            props => (
+                <div className="mt-4 table-responsive custom-table custom-table-responsive">
+                  <BootstrapTable
+                    { ...props.baseProps }
+                    bordered={false}
+                    pagination={paginationFactory({ sizePerPage: 10 })}
+                  />
+                </div>
+            )
+          }
+        </ToolkitProvider>
+    );
   }
 
-  return(
-    <ToolkitProvider
-        keyField="id"
-        data={executions}
-        columns={columns}
-        >
-        {
-          props => (
-              <div className="mt-4 table-responsive custom-table custom-table-responsive">
-                <BootstrapTable
-                  { ...props.baseProps }
-                  bordered={false}
-                  pagination={paginationFactory({ sizePerPage: 10 })}
-                />
-              </div>
-           )
-        }
-      </ToolkitProvider>
-    );
+
 };
 
 export default ExecutionsTable;
