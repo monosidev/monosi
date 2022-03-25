b44b1a2e3e12649e257bacd92f6551206542954c
Ivan Porollo <iporollo@gmail.com>
2022-03-25 00:55:04 -0700

    add support for bigquery datasource in UI

--- src/ui/src/components/forms/DatasourceForm/index.tsx
+++ src/ui/src/components/forms/DatasourceForm/index.tsx
@@ -5,9 +5,9 @@ import {
   EuiFormRow,
   EuiHeaderLogo,
   EuiButton,
-  EuiSuperSelect,
   EuiFieldText,
   EuiFieldPassword,
+  EuiFilePicker,
   EuiSpacer,
   EuiFlexGrid,
   EuiCard,
@@ -16,12 +16,14 @@ import {
   EuiPageHeader,
 } from '@elastic/eui';
 
+import { BigQueryLogo } from 'images';
 import datasourceService from 'services/datasources';
 
 enum DataSourceTypes {
   SNOWFLAKE = 'snowflake',
   POSTGRESQL = 'postgresql',
   REDSHIFT = 'redshift',
+  BIGQUERY = 'bigquery'
 }
 
 const DatasourceForm = () => {
@@ -38,48 +40,86 @@ const DatasourceForm = () => {
   const [schema, setSchema] = useState<string>('');
   const [host, setHost] = useState<string>('');
   const [port, setPort] = useState<string>('');
+  const [project, setProject] = useState<string>('');
+  const [dataset, setDataset] = useState<string>('');
+  const [file, setFile] = useState<File | null>(null);
 
   const submitForm = async () => {
-    let body;
+
+    let config = {}
+
     if (datasourceType === DataSourceTypes.SNOWFLAKE) {
-      body = {
-        name: datasourceName,
-        type: datasourceType,
-        config: {
-          driver: datasourceType,
-          account: account,
-          user: user,
-          password: password,
-          warehouse: warehouse,
-          database: database,
-          schema: schema,
-        },
+      config = {
+        driver: datasourceType,
+        account: account,
+        user: user,
+        password: password,
+        warehouse: warehouse,
+        database: database,
+        schema: schema,
       };
     } else if (
       datasourceType === DataSourceTypes.POSTGRESQL ||
       datasourceType === DataSourceTypes.REDSHIFT
     ) {
-      body = {
-        name: datasourceName,
-        type: datasourceType,
-        config: {
-          user: user,
-          password: password,
-          host: host,
-          port: parseInt(port),
-          database: database,
-          schema: schema
-        },
+      config = {
+        user: user,
+        password: password,
+        host: host,
+        port: parseInt(port),
+        database: database,
+        schema: schema
+      };
+    } else if (datasourceType === DataSourceTypes.BIGQUERY) {
+      // TODO: Implement better validation
+      if(!datasourceName || !project || !dataset || !file) return;
+
+      const credentials_base64 = await toBase64(file);
+
+      config = {
+        project,
+        dataset,
+        credentials_base64,
       };
     }
+
+    const body = {
+      name: datasourceName,
+      type: datasourceType,
+      config
+    };
+
     await datasourceService.create(body);
 
     window.location.reload();
+    
   };
 
   const onChange = (value: any) => {
     setDatasourceType(value);
   };
+
+  const onFileChange = (files: FileList | null) => {
+    if(files && files.length > 0) {
+      const filesArray = Array.from(files);
+      const firstFile = filesArray[0];
+      setFile(firstFile);
+    }
+  };
+
+  const toBase64 = (file: File):Promise<string> => new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      let encoded = reader.result?.toString() || '';
+      encoded = encoded.replace(/^data:(.*,)?/, '');
+      if ((encoded.length % 4) > 0) {
+        encoded += '='.repeat(4 - (encoded.length % 4));
+      }
+      resolve(encoded);
+    };
+    reader.onerror = error => reject(error);
+  });
 
   return (
     <div>
@@ -118,6 +158,18 @@ const DatasourceForm = () => {
             icon={<EuiIcon type="logoAWS" size="xl" />}
             title="Redshift"
             description="Connect to AWS Redshift Data Warehouse"
+          />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiCard
+            selectable={{
+              onClick: () => onChange(DataSourceTypes.BIGQUERY),
+              isSelected: datasourceType === DataSourceTypes.BIGQUERY,
+              isDisabled: false,
+            }}
+            icon={<EuiIcon type={BigQueryLogo} size="xl" />}
+            title="BigQuery"
+            description="Connect to Google BigQuery Data Warehouse"
           />
         </EuiFlexItem>
       </EuiFlexGrid>
@@ -297,6 +349,46 @@ const DatasourceForm = () => {
               placeholder="tpch_sf1000"
               onChange={(e) => setSchema(e.target.value)}
               value={schema}
+            />
+          </EuiFormRow>
+        </div>
+      )}
+      {datasourceType === DataSourceTypes.BIGQUERY && (
+        <div>
+          <EuiPageHeader
+            iconType="https://cdn.worldvectorlogo.com/logos/google-bigquery-logo-1.svg"
+            pageTitle="BigQuery"
+            description="Connect to Google BigQuery Data Warehouse"
+          />
+          <EuiHorizontalRule />
+          <EuiFormRow label="Name for Data Source">
+            <EuiFieldText
+              placeholder="Company Data Warehouse"
+              onChange={(e) => setDatasourceName(e.target.value)}
+              value={datasourceName}
+            />
+          </EuiFormRow>
+          <EuiFormRow label="Project">
+            <EuiFieldText
+              placeholder="abc123"
+              onChange={(e) => setProject(e.target.value)}
+              value={project}
+            />
+          </EuiFormRow>
+          <EuiFormRow label="Dataset">
+            <EuiFieldText
+              placeholder="mydataset"
+              onChange={(e) => setDataset(e.target.value)}
+              value={dataset}
+            />
+          </EuiFormRow>
+          <EuiFormRow label="Service Account JSON File">
+            <EuiFilePicker
+              initialPromptText="Select Service Account JSON file"
+              onChange={onFileChange}
+              display='default'
+              aria-label="Service Account JSON File"
+              accept='.json'
             />
           </EuiFormRow>
         </div>
