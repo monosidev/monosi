4d30a846406197b54d42be395257a3d58b70c608
Ivan Porollo <iporollo@gmail.com>
2022-03-25 00:49:24 -0700

    add bigquery logo & update image folder structure

---
+++ src/ui/src/images/index.ts
@@ -0,0 +1,5 @@
+import PagerDutyLogo from './PagerDutyLogo';
+import WebhookLogo from './WebhookLogo';
+import BigQueryLogo from './BigQueryLogo';
+
+export { PagerDutyLogo, WebhookLogo, BigQueryLogo };
