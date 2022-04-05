92875ff956714925b7b2fa8ca2fdbc90a3aa0f8e
Kevin Unkrich <kevin@unkri.ch>
2022-04-05 10:34:34 -0400

    Query to time series -ify metrics responses

--- src/server/handlers/metrics.py
+++ src/server/handlers/metrics.py
@@ -5,8 +5,46 @@ from sqlalchemy import func
 from server.models import Metric, ZScore
 from server.middleware.db import db
 
+
 from .monitors import MonitorResource
 
+
+# TODO: Do not format directly
+METRIC_SQL_TS_DATA = """
+SELECT * FROM (
+    SELECT 
+        value_partition, 
+        hours as time_window_start, 
+        hours + '1 HOUR'::INTERVAL as time_window_end, 
+        first_value(value) over (partition by value_partition order by hours DESC) value,
+        first_value(zscore) over (partition by value_partition order by hours DESC) zscore,
+        first_value(expected_range_start) over (partition by value_partition order by hours DESC) expected_range_start,
+        first_value(expected_range_end) over (partition by value_partition order by hours DESC) expected_range_end,
+        first_value(error) over (partition by value_partition order by hours DESC) error
+    FROM (
+        SELECT
+          MSI_METRICS.*,
+          MSI_ZSCORES.*,
+          hours,
+            sum(case when value is null then 0 else 1 end) over (order by hours DESC) as value_partition
+          FROM GENERATE_SERIES((
+            SELECT DATE_TRUNC('HOUR', MIN(created_at - '1 DAY'::INTERVAL)) FROM MSI_MONITORS WHERE
+                MSI_MONITORS.table_name ilike '{table_name}' AND
+                MSI_MONITORS.database ilike '{database}' AND
+                MSI_MONITORS.schema ilike '{schema}')
+          , CURRENT_DATE, '1 HOUR'::INTERVAL) hours
+          LEFT JOIN MSI_METRICS ON DATE_TRUNC('HOUR', MSI_METRICS.time_window_start) = hours
+          AND
+            MSI_METRICS.table_name ilike '{table_name}' AND
+            MSI_METRICS.database ilike '{database}' AND
+            MSI_METRICS.schema ilike '{schema}' AND
+            MSI_METRICS.column_name ilike '{column_name}' AND
+            MSI_METRICS.metric ilike '{metric}'
+          LEFT JOIN MSI_ZSCORES ON MSI_ZSCORES.METRIC_ID = MSI_METRICS.ID
+          ORDER BY hours DESC
+    ) as metrics
+) as final WHERE value is not null;
+"""
 
 
 # class MetricListResource(Resource):
@@ -27,6 +65,8 @@ from .monitors import MonitorResource
 #         obj = self._retrieve_by_kwargs(table, database, schema)
 
 #         return {self.key: obj}
+
+
 
 
 class MetricListResource(Resource):
@@ -71,14 +111,26 @@ class MetricListResource(Resource):
 
     def _retrieve_detailed(self, table_name, database, schema, column_name, metric):
         try:
-            objs = db.session.query(Metric, ZScore).outerjoin(ZScore, ZScore.metric_id == Metric.id).filter(
-                Metric.table_name==table_name,
-                Metric.database==database,
-                Metric.schema==schema,
-                Metric.metric == metric,
-                Metric.column_name == column_name
-                ).order_by(Metric.time_window_end).all() # TODO: ORDER BY
-            metrics = [(lambda d: (d.update(obj[1].to_dict()) or d) if obj[1] else d)(obj[0].to_dict()) for obj in objs]
+            query = METRIC_SQL_TS_DATA.format(
+                table_name=table_name,
+                database=database,
+                schema=schema,
+                column_name=column_name,
+                metric=metric
+            )
+            print(query)
+            objs = db.session.execute(query)
+            metrics = [
+                {
+                    'time_window_start': str(obj[1]),
+                    'time_window_end': str(obj[2]),
+                    'value': obj[3],
+                    'zscore': obj[4],
+                    'expected_range_start': obj[5],
+                    'expected_range_end': obj[6],
+                    'error': obj[7]
+                }
+            for obj in objs]
         except Exception as e:
             logging.warn(e)
             abort(404)
