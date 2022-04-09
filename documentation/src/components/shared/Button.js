2ba40082360f7df178771ca3a6d1f255b0a246da
Kevin Unkrich <kevin@unkri.ch>
2022-04-09 13:22:53 -0400

    Update docs colors

--- documentation/src/components/shared/Button.js
+++ documentation/src/components/shared/Button.js
@@ -5,7 +5,7 @@ export const Button = ({children, type, name, className}) => {
     <button
       type={type}
       name={name}
-      className={`flex items-center justify-center rounded-md border border-transparent bg-[color:var(--ifm-color)] px-5 py-3 text-base font-medium  text-[color:var(--ifm-background-color)]
+      className={`flex items-center justify-center rounded-md border border-transparent bg-[color:var(--ifm-color-highlight)] px-5 py-3 text-base font-medium  text-[color:var(--ifm-background-color)]
       hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-[color:var(--ifm-color)] focus:ring-offset-2 ${className}`}
     >
       {children}
